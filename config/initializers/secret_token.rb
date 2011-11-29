f20b0187f7bb45f29b4185229a1f3e738becb5e1
hsoans <hsoans@yahoo.com>
2011-11-29 13:37:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ptdemo::Application.config.secret_token = 'b17a336d29a68ec2d7faa27cfb025a740c633caf5b0cf7de4e91ec0d20d775cda54c135ff037a8c961cec6d3839d8986a4f2735e11fe4190481e00dd81c983bb'
